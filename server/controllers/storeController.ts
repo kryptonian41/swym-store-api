a918af6a17fc8b24919ebe579dc4ecfd7dcbc872
apoorvHearth <apoorv@heyhearth.com>
2020-08-12 23:15:10 +0530

    Error handling for add new store feature

--- server/controllers/storeController.ts
+++ server/controllers/storeController.ts
@@ -1,6 +1,7 @@
 import { createNewEmptyStore } from '../services/StoreService'
 import { TaskQueue } from '@/commons/TaskQueue'
 import TaskTypes from '@/commons/TaskTypes'
+import Store from '@/commons/mongoDb/models/StoreModel'
 
 export const addNewStore = async (req, res) => {
   const { domain } = req.body
@@ -9,6 +10,15 @@ export const addNewStore = async (req, res) => {
       message: 'Missing required request parameteres',
     })
   await createNewEmptyStore(domain)
-  TaskQueue.add(TaskTypes.ADD_NEW_STORE, { domain, source: 'builtWith' })
-  res.json({ success: true })
+  let store = await Store.findOne({ domain })
+  if (!store)
+    store = await Store.create({
+      domain,
+      dateAdded: new Date(),
+      isPopulated: false,
+      lastUpdated: new Date(),
+    })
+  if (!store.isPopulated)
+    TaskQueue.add(TaskTypes.ADD_NEW_STORE, { domain, source: 'builtWith' })
+  res.json({ success: true, data: store })
 }
